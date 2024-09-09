f8cd015f8ff0a7d1bd5a00a21d27c6f451a39902
xiaozhang <xiaozhang@123.com>
2024-09-09 15:56:21 +0800

    support mlu devices

--- tutorial_train.py
+++ tutorial_train.py
@@ -1,3 +1,11 @@
+
+import torch
+try:
+    import torch_mlu
+    import torch_mlu.utils.gpu_migration
+except Exception as e:
+    print(f"failed import torch_mlu!!! error {e}")
+
 from share import *
 
 import pytorch_lightning as pl
@@ -9,7 +17,7 @@ from cldm.model import create_model, load_state_dict
 
 # Configs
 resume_path = './models/control_sd15_ini.ckpt'
-batch_size = 4
+batch_size = 2
 logger_freq = 300
 learning_rate = 1e-5
 sd_locked = True
@@ -28,7 +36,9 @@ model.only_mid_control = only_mid_control
 dataset = MyDataset()
 dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
+trainer = pl.Trainer(precision=16,
+                    strategy='ddp_find_unused_parameters_true', 
+                    callbacks=[logger])
 
 
 # Train!
